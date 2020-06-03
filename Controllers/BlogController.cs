081058335004ca6e07770de93d79010dd420bc0b
Alan West <west.t.alan@gmail.com>
2020-06-03 17:51:05 -0400

    3 chaps

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCalifornia.Controllers
+{
+    [Route("blog")]
+    public class BlogController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route(@"{year:min(2000)}/{month:range(1,12)}/{key}")]
+        public IActionResult Post(int year, int month, string key)
+        {
+            var post = new Post
+            {
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "Alan West",
+                Body = "This is a great blog post, don't you think?"
+            };
+
+            return View(post);
+        }
+    }
+}
