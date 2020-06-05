b09a440980b9364714e1062bf4d233fc95554f48
Alan West <west.t.alan@gmail.com>
2020-06-05 18:14:47 -0400

    almost working

--- Startup.cs
+++ Startup.cs
@@ -6,24 +6,28 @@ using ExploreCalifornia.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace ExploreCalifornia
 {
     public class Startup
     {
         private readonly IConfiguration configuration;
+
         public Startup(IConfiguration configuration)
         {
             this.configuration = configuration;
         }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<FormattingService>();
+
             services.AddTransient<FeatureToggles>(x => new FeatureToggles
             {
                 DeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions")
@@ -35,12 +39,22 @@ namespace ExploreCalifornia
                 options.UseSqlServer(connectionString);
             });
 
+            services.AddDbContext<IdentityDataContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("IdentityDataContext");
+                options.UseSqlServer(connectionString);
+            });
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDataContext>();
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        [Obsolete]
         public void Configure(
-            IApplicationBuilder app, 
+            IApplicationBuilder app,
             IHostingEnvironment env,
             FeatureToggles features)
         {
@@ -59,12 +73,14 @@ namespace ExploreCalifornia
                 await next();
             });
 
+            app.UseIdentity();
+
             app.UseMvc(routes =>
             {
-                routes.MapRoute("Default", 
-                    "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute("Default",
+                    "{controller=Home}/{action=Index}/{id?}"
+                );
             });
-
 
             app.UseFileServer();
         }
